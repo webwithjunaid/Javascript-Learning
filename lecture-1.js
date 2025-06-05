7f0520a8a529597f81f12bb8b6ee2c6cc31be9d4
Junaid Shah <codedbyjunaid@gmail.com>
2025-06-04 19:59:24 -0700

    Add files via upload

---
+++ lecture-1.js
@@ -0,0 +1,82 @@
+// Variable
+
+// There are there ways to declare variables
+
+// 1: let
+// 2: var
+// 3: const
+
+
+// let myName = "Junaid"
+// console.log(myName)
+
+// var myAge = 24;
+// console.log(myAge)
+
+// const marks = 944;
+// console.log(marks)
+
+// we use template litterals for print variables for example
+
+// let food = "Biryani";
+
+// console.log(`I love ${food}`);
+
+
+// Operators
+
+// there are two types of operators " Uninary Operator or Binary Operator "
+
+// Unary operator
+
+// let a = 5;
+// a++;
+// console.log(a);
+
+// unary operator work with one operand
+
+
+// Binary Operator
+
+// let b = 4;
+// b = b + 4;
+// console.log(b);
+
+
+// let a = 5;
+// let b = 2;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a ** b);
+// console.log(a % b);
+
+
+// Data Types
+
+// there are different types of data types in javascript here are some
+
+// Primitive Data SVGUnitTypes;
+
+// String
+// Number
+// Boolean
+// Undefined
+// Null
+
+// Reference Data Types
+
+// Objects
+// Arrays
+// Function
+
+
+// How to fined data type in js
+
+// let a = "Junaid";
+// let b = 24;
+
+// console.log(typeof a);
+// console.log(typeof b);
