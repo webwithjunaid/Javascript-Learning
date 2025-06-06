4c75511857d2cede0eb25b629f17a2ac4e616176
Junaid Shah <codedbyjunaid@gmail.com>
2025-06-05 19:59:38 -0700

    Add files via upload

---
+++ lecture-2.js
@@ -0,0 +1,15 @@
+// String Function
+
+let name = "Junaid";
+let para = "Hello, how are you?"
+let trim = "    hi how are you"
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(name.indexOf('u'));
+console.log(para.includes('how'));
+console.log(para.slice(0, 5));
+console.log(para.substr(0, 5));
+console.log(para.replace('how', 'who'));
+console.log(para.split('--'));
+console.log(trim.trim());
+
