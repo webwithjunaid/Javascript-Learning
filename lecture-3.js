30d36413ab8e7c5fd691116d154a53c771e64990
Junaid Shah <codedbyjunaid@gmail.com>
2025-06-05 20:02:12 -0700

    Add files via upload

---
+++ lecture-3.js
@@ -0,0 +1,68 @@
+// 1. Even or Odd
+
+// let number = prompt("Enter your number: ")
+
+// if(number%2==0){
+//     console.log("Even");
+    
+// }else{
+//     console.log("Odd");
+    
+// }
+
+
+// 2. Check Driving Eligibility
+
+// let age = prompt("Enter your age: ")
+
+// if(age>=18){
+//     console.log("You can drive");
+    
+// }else{
+//     console.log("You can Not drive");
+    
+// }
+
+// 3. Number Comparison
+
+// let a = 10;
+// let b = 20;
+
+// if(a>b){
+//     log("a is greater")
+// }else{
+//     console.log("b is greater");
+    
+// }
+
+
+// 4. Password Checker
+
+// let Password = prompt("Enter your password");
+
+// if (Password=="admin123") {
+//     console.log("Log in Successfull");
+    
+// }else{
+//     console.log("Wrong password! Please try again.");
+    
+// }
+
+
+// 5. Simple Grading System
+
+// let score = prompt("Enter your score: ");
+
+// if(score>=90){
+//     console.log("A");
+    
+// }else if(score>=80 && score<=89){
+//     console.log("B");
+    
+// }else if(score>=70 && score<=79){
+//     console.log("C");
+    
+// }else{
+//     console.log("F");
+    
+// }
