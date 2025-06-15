9f28a44ef797840d53b569b0fe1e8cfb971977d3
Junaid Shah <codedbyjunaid@gmail.com>
2025-06-15 20:17:47 +0500

    Add files via upload

---
+++ Lecture-7.js
@@ -0,0 +1,104 @@
+// For Loop Questions
+
+// 1. Print Numbers 1 to 10
+
+// for(let i = 1; i<=10; i++){
+//     console.log(i);
+    
+// }
+
+
+// 2. Print Even Numbers 1 to 20
+
+// for(let i = 2; i<=20; i = i+2){
+//     console.log(i);
+    
+// }
+
+
+// 3. Sum of Numbers 1 to 100
+
+// let sum = 0;
+
+// for(let i = 1; i<=100; i++){
+//     sum = sum + i;
+// }
+// console.log(`The sum of numbers from 1 to 100 is: ${sum}`);
+
+
+// 4. Multiplication Table of 5
+
+// let number = 5;
+
+// for(let i = 1; i<=10; i++){
+//   console.log(`${number} x ${i} = ${number * i}`);
+  
+    
+// }
+
+
+// 5. Countdown from 10 to 1
+
+// for(let i = 10; i >= 1; i--){
+//     console.log(i);
+    
+// }
+
+
+
+                                    // While Loop Questions
+
+// 1. Print Numbers 1 to 10
+
+// let number = 1; 
+
+// while (number<=10) {
+//     console.log(number);
+//     number++
+// }
+
+
+// 2. Sum of Even Numbers 1 to 50
+
+// let number = 1;
+// let sum = 0;
+
+// while (number<=50) {
+//     sum = sum + number;
+//     number++;
+// }
+// console.log(`The sum of number from 1 to 50 is: ${sum}`);
+
+
+
+// 3. Guess the Number Game
+
+// let secNumber = 7;
+// let guess = prompt("Guess the number from 1 to 10: ")
+
+// while (parseInt(guess) !== secNumber) {
+//     guess = prompt("You enter incorrect number Please try again: ")
+// }
+
+// console.log(alert("Congratulation! you guess the correct number ✅"));
+
+
+// 4. Reverse a Number
+
+
+// let number = prompt("Enter a number:");
+// let reverse = 0;
+
+// number = parseInt(number);
+
+// while (number !== 0) {
+//     let digit = number % 10;
+//     reverse = reverse * 10 + digit;
+//     number = Math.floor(number / 10);
+// }
+
+// console.log("Reversed number is:", reverse);
+
+
+
+
