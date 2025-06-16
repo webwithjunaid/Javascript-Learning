6fffe92dbab717d3d0f5040acf79b740d173003a
Junaid Shah <codedbyjunaid@gmail.com>
2025-06-16 07:39:07 +0500

    Add files via upload

---
+++ lecture-8.js
@@ -0,0 +1,80 @@
+// 1. Simple Greeting Function
+
+// function greeting(name) {
+//     console.log(`Hell, ${name}`);
+    
+// }
+// greeting("Junaid")
+
+
+
+// 2. Add Two Numbers
+
+// function sum(a,b) {
+//     console.log(a+b);
+    
+// }
+// sum(2,3)
+
+
+
+// 3. Even or Odd
+
+// function isEven(number) {
+//     if(number % 2 === 0){
+//         console.log("Even");
+        
+//     }else{
+//         console.log("Odd");
+        
+//     }
+// }
+
+// isEven(5)
+
+
+
+// 4. Square of a Number
+
+// function square(number) {
+//     return number * number;
+// }
+
+// console.log(square(4));
+
+
+
+// 5. Calculate Area of a Rectangle
+
+
+// function areaofRectangle(width,height) {
+//     area = width * height;
+//     console.log(area);
+    
+// }
+// areaofRectangle(4,5)
+
+
+
+
+// 6. Calculater Function
+
+// function calculator(num1, num2, operator) {
+//     if (operator === "+") {
+//         return num1 + num2;
+//     } else if (operator === "-") {
+//         return num1 - num2;
+//     } else if (operator === "*") {
+//         return num1 * num2;
+//     } else if (operator === "/") {
+//         if (num2 !== 0) {
+//             return num1 / num2;
+//         } else {
+//             return "Cannot divide by zero";
+//         }
+//     } else {
+//         return "Invalid operator";
+//     }
+// }
+
+// console.log(calculator(4,5,"+"));
