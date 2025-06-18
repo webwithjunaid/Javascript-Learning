23444dbeaf2354499266c8dd67ef12db22d4b05d
Junaid Shah <codedbyjunaid@gmail.com>
2025-06-18 18:18:59 +0500

    Update lecture-9.js

--- lecture-9.js
+++ lecture-9.js
@@ -1,3 +1,4 @@
+                            ******************* Todo App *************************
 let todo = [];
 let request = prompt("add,remove,list")
 
@@ -20,3 +21,18 @@ while (request !== "quit") {
 }
 
 console.log(todo);
+
+                                ***************** Random Quotes Generator ************
+
+const quotes = ["“To be able to forgive we must first meet the legitimate needs of the s of us that don’t want to forgive.”",
+                "“We need to be willing to accept our feelings around an event in order to fully gain the benefits, which are available from it.”",
+                "“If we hold onto our wounds in an area of life then we hold back the growth of wisdom in that area too.”",
+                "“A more forgiving attitude makes it easier to develop wisdom…Likewise a deeper capacity for wisdom makes it easier to forgive.”",
+                "“A garden emerges from tending the outer world; forgiveness emerges from tending our inner world.”"
+]
+
+function quotegenerator() {
+    let index = Math.floor(Math.random()*quotes.length);
+    return quotes[index];
+}
+console.log(quotegenerator());
